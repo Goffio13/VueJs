51dd0642c778e6b0331c2f0d9fc5b85aa680f333
Goffio <92335405+Goffio13@users.noreply.github.com>
2023-03-06 17:38:49 +0100

    VueJS

---
+++ VueJs/index.js
@@ -0,0 +1,23 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue!',
+        vu: false
+    }
+})
+var rouge = new Vue({
+    el: '#rouge',
+    data: {
+        message: 'Je suis rouge',
+        color: 'rouge'
+    }
+})
+var bleu = new Vue({
+    el: '#bleu',
+    data: {
+        message : 'Je suis bleu',
+        color: 'bleu'
+    }
+})
+;
+
---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+      <div class="container-fluid">
+        <nav class="navbar navbar-expand-lg sticky-top navbar-dark bg-dark">
+            <span class="navbar-brand">VIE-IE-ED</span>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <router-link to="/"> <a class="nav-link">Acceuil</a></router-link> 
+                    </li>
+                    <li class="nav-item">
+                         <router-link to="/import"><a class="nav-link">Import</a></router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link to="/export"> <a class="nav-link">Export</a></router-link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
